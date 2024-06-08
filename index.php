4dfd8dc7d7d3dea75025250f3925a0240e406db2
itzsebas121 <sebastipan121@gmail.com>
2024-06-08 13:22:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="Css/main.css" />
+    <link rel="stylesheet" href="main.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="Css/index.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
